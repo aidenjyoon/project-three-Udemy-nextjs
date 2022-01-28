a848032634d9b245061946582e5c7a9bca14c5b6
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2022-01-28 15:52:49 -0800

    setup the useRef to receive the feedback for backend

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
+import { useRef } from "react";
+
 const HomePage = () => {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  };
+
   return (
     <>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your Feedback</label>
-          <textarea id="feedback" rows="5"></textarea>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
         </div>
         <div>
           <button>Send Feedback</button>
